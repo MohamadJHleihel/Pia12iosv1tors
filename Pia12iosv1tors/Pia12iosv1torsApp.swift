ccf0f322af878cd1dff4fdda3cb8f47bc8782432
Mohamad Hleihel <mohammad.helehel@hotmail.com>
2023-11-08 08:27:27 +0100

    Initial Commit

---
+++ Pia12iosv1tors/Pia12iosv1torsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pia12iosv1torsApp.swift
+//  Pia12iosv1tors
+//
+//  Created by Mohamad Hleihel on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct Pia12iosv1torsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
